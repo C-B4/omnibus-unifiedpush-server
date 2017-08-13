7e37c3f496a631011846662175b598cb6d911d37
yaniv-cb4 <yanivn@c-b4.com>
2017-08-13 18:32:14 +0300

    Test new APNS + Matrix service

--- config/software/unifiedpush-server.rb
+++ config/software/unifiedpush-server.rb
@@ -25,7 +25,7 @@ dependency "wildfly"
 
 <<<<<<< HEAD
 version "1.2.0-SNAPSHOT" do
-  source md5: "acdf4355512cb3fb6af2d5dd0d11295c"
+  source md5: "2747c7ff6552614cd1603683bbc86322"
 end
 ||||||| parent of 17167ac... Remove unused sources
 source git: "https://github.com/aerobase/unifiedpush-server.git"
