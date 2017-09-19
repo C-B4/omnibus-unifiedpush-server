74cdc483f21ec23a69aa337e0c20e3a84684e6b7
yaniv-cb4 <yanivn@c-b4.com>
2017-09-19 11:27:48 +0300

    Fix downloading sources from git

--- config/software/unifiedpush-server.rb
+++ config/software/unifiedpush-server.rb
@@ -24,6 +24,8 @@ dependency "postgresql"
 
 source git: "https://github.com/C-B4/unifiedpush-server.git"
 
+source git: "https://github.com/aerobase/unifiedpush-server.git"
+
 relative_path "unifiedpush-server"
 build_dir = "#{project_dir}"
 
