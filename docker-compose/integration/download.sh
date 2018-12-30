f5590b78ab0b7d58094f09844f9b5d77860a6468
Yaniv Marom Nachumi <yanivn@c-b4.com>
2018-12-30 19:19:04 +0200

    CAPP-10819 - Build docker with snapshot version

--- docker-compose/integration/download.sh
+++ docker-compose/integration/download.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-curl --ssl -u "cb4:Cb4P@ssw0rd" -O ftp://us-ftp.c-b4.com/ups/unifiedpush-server-1.2.8-Final.1.el7.x86_64.rpm
+curl --ssl -u "cb4:Cb4P@ssw0rd" -O ftp://us-ftp.c-b4.com/ups/unifiedpush-server-1.2.8~Final.1.el7.x86_64.rpm
 curl --ssl -u "cb4:Cb4P@ssw0rd" -O ftp://us-ftp.c-b4.com/ups/keycloak-server-1.2.8-Final.1.el7.x86_64.rpm
